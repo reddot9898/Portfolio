e90cbc2ea63259e39701b5c06531f1313cfb612d
randyn.collins@icloud.com <you@example.com>
2023-09-19 14:57:18 -0400

    fixed dependencie issues, compiling now

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom'; // Import createRoot from react-dom
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root')); // Create a root instance
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
